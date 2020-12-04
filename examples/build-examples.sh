59445e17a4ae08cbd642c3e7317a269363752966
Wandmalfarbe <Wandmalfarbe@users.noreply.github.com>
2020-12-04 17:32:15 +0100

    resource path

--- examples/build-examples.sh
+++ examples/build-examples.sh
@@ -8,7 +8,7 @@ echo "##"
 echo ""
 
 echo "- header-and-footer"
-pandoc "header-and-footer/header-and-footer.md" -o "header-and-footer/header-and-footer.pdf" --from markdown --template "../eisvogel.latex" --listings --resource-path "./header-and-footer/"
+pandoc "header-and-footer/header-and-footer.md" -o "header-and-footer/header-and-footer.pdf" --from markdown --template "../eisvogel.latex" --listings --resource-path ".:./header-and-footer/"
 pdftoppm -r 150 -png "header-and-footer/header-and-footer.pdf" > "header-and-footer/header-and-footer.png"
 
 rm "../eisvogel.latex"
